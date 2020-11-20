079396ccf733a318dedcdb23e311589a94656ce1
Lee Burgess <lee.j.burges77@gmail.com>
2020-11-20 09:47:24 +0000

    change

--- backend/config/environment.js
+++ backend/config/environment.js
@@ -1,7 +1,4 @@
 
-// ! Any other config variables that you used can stay in here, make sure you are still exporting them at the bottom.
-const secret = 'This is a really long secret string no one is going to guess 3onsfdnisdo.'
-
 // ! This gives us the power to customize our port with heroku
 const port = process.env.PORT || 8000
 
@@ -10,12 +7,12 @@ const env = process.env.NODE_ENV || 'development'
 
 // ! This will allow us to use a different url for heroku (which will point to mongo atlas)
 const dbURI = env === 'production'
-? process.env.MONGODB_URI
-: `mongodb://localhost/pokedb-${env}`
+  ? process.env.MONGODB_URI
+  : `mongodb://localhost/pubsdb-${env}`
 
 const secret = 'This is our secret to be moved to another folder'
 
 // ! Export everything
 module.exports = {
-secret, port, dbURI
+  secret, port, dbURI
 }
